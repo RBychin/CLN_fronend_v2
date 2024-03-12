42d7badf303132b5332855e26bda0216cb7f8b31
Roman <r.bychine@gmail.com>
2024-03-12 17:35:23 +0300

    lalala

--- src/utills/config.jsx
+++ src/utills/config.jsx
@@ -1,6 +1,6 @@
 export const Config = {
     apiToken: '52053baf54a991142b6af804e9de8a602cd4c297',
-    apiUrl: 'https://cbc6-79-98-8-28.ngrok-free.app/api',
+    apiUrl: 'http://127.0.0.1:8000/api',
     telegram_id: 22222,
     debug: true
 }
